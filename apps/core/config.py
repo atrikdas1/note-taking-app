eb96cf76e560c053bc70993f74e71b713f3fa895
Atrik <atrik@usec.io>
2022-11-23 22:58:38 +0800

    Bug: Fix connecting to db

--- apps/core/config.py
+++ apps/core/config.py
@@ -4,11 +4,13 @@ This file contains all the constants which can be configured according to the us
 
 import sqlalchemy
 
+CURRENT_VERSION_API = "v1"
+
 SQLALCHEMY_DATABASE_URI = sqlalchemy.engine.url.URL.create(
     drivername="postgresql",
     username="postgres",
     password="postgres",
     host="note-taker-db",
     port=5432,
-    database="note-taker",
+    database="postgres",
 )
