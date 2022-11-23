4f92a1e5156f7762aeb47ad59dda850f581556f9
Atrik <atrik@usec.io>
2022-11-23 23:01:15 +0800

    Feat: Complete post new note endpoint

---
+++ apps/notes_api/query.py
@@ -0,0 +1,34 @@
+"""
+Functions that help query the database for certain data
+"""
+
+import logging
+from typing import Dict, List, Tuple
+from notes_api.models import Note
+from core import config
+from core.db import db
+
+logger = logging.getLogger(f"NOTES_API.{__name__}")
+
+
+def filter_notes_by_id(note_id: int) -> Tuple[int, str, List]:
+    try:
+        notes = (
+            db.session.query(Note).filter(
+                Note.id == note_id
+            ).all()
+        )
+        notes_list = []
+        if len(notes) > 0:
+            for note in notes:
+                notes_list.append(note.serialize())
+
+        return (
+            200,
+            None,
+            notes_list,
+        )
+    except Exception as e:
+        err_msg = f"note with ID {note_id} not found"
+        logger.exception(e)
+        return (500, err_msg, None)
---
+++ apps/notes_api/resources.py
@@ -0,0 +1,73 @@
+"""Logic for RESTFUL APIs of notes"""
+
+import logging
+
+from notes_api import models
+from notes_api import query
+from notes_api.schemas import CreateNoteSchema
+from core import utils as apputils
+from core.db import db
+from flask import jsonify, make_response, request
+from flask_restful import Resource
+from marshmallow import ValidationError
+
+logger = logging.getLogger(f"NOTES_API.{__name__}")
+
+
+class Notes(Resource):
+    """
+    Handle the following requests:
+    1. POST Create a basic note
+    2. GET Get all the notes in the db
+    3. DELETE Remove all the notes in the db
+    """
+
+    def post(self):
+        """Create a basic note"""
+        # Check if request contains a json payload
+        try:
+            json_data = request.get_json()
+        except Exception as e:
+            msg = "Notes.post(): No JSON payload found."
+            logger.exception(f"{e}:{msg}")
+            return apputils.custom_abort(400, "Bad Request", msg)
+
+        # Validate CreateNoteSchema request json
+        try:
+            logger.debug(f"CreateNoteSchema json_data: {json_data}")
+            template_schema = CreateNoteSchema()
+            data = template_schema.load(json_data)
+        except ValidationError as err:
+            logger.warning(
+                f"Notes.post(): request with invalid data. Details: {err.messages}"
+            )
+            return apputils.custom_abort(
+                400,
+                "Bad Request",
+                f"Please correct the highlighted errors and try again: {err.messages}",
+            )
+
+        # Creating a new note
+        try:
+            new_note = models.Note(
+                content=data["content"],
+                tags=data["tags"],
+            )
+            db.session.add(new_note)
+            db.session.flush()
+
+            logger.debug(f"new_note.id: {new_note.id}")
+
+            response_code, response_msg, note_list = query.filter_notes_by_id(new_note.id)
+            if note_list is None:
+                return apputils.custom_abort(response_code, "Internal Server Error", "")
+            if len(note_list) == 0:
+                return apputils.custom_abort(404, "Not Found", "")
+            result = note_list[0]
+            logger.info(f"Notes.post(): Created new note. Details: {result}")
+            db.session.commit()
+
+            return make_response(jsonify(result), 201)
+        except Exception as e:
+            logger.exception(f"Notes.post(): Internal Server Error. Errors: {e}.")
+            return apputils.custom_abort(500, "Internal Server Error", "")
