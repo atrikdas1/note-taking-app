c287d44ee783f6ce69b1829785bf3466651e8d55
Atrik <atrik@usec.io>
2022-11-23 22:59:18 +0800

    Feat: Create note model and generate alembic revision

---
+++ apps/notes_api/models.py
@@ -0,0 +1,39 @@
+from database.models import Base
+from sqlalchemy import (
+    Column,
+    Integer,
+    PrimaryKeyConstraint,
+    String
+)
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy_json import mutable_json_type
+
+class Note(Base):
+    """Model for a note"""
+
+    __tablename__ = "notes"
+    __table_args__ = (PrimaryKeyConstraint("id"),)
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        comment="id of the note",
+    )
+
+    content = Column(String, nullable=False, comment="content of the note")
+
+    tags = Column(
+        mutable_json_type(dbtype=JSONB, nested=True),
+        nullable=False,
+        comment="JSON formatted array of tags, each one is a string",
+        server_default='["random"]',
+    )
+
+    def serialize(self):
+        """Create dictionary containing Note info."""
+        out = dict()
+        out["id"] = int(self.id)
+        out["content"] = str(self.content)
+        out["tags"] = self.tags
+        return out
