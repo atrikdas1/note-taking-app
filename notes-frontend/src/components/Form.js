27c61c8b0cb55ac2452a4d29dfee5e104fa2fee2
Atrik <atrik@usec.io>
2022-11-27 20:30:00 +0800

    Chore: Error handling

--- notes-frontend/src/components/Form.js
+++ notes-frontend/src/components/Form.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import APIService from '../components/APIService';
+import Alert from 'react-bootstrap/Alert';
 
 function Form(props) {
     const [content, setContent] = useState(props.note.content);
     const [tags, setTags] = useState(props.note.tags);
+    const [showAlert, setShowAlert] = useState(false);
 
     const openForm = () => {
         props.setEditedNote({content:'', tags:''})
@@ -14,20 +16,35 @@ function Form(props) {
         setTags(props.note.tags)
     }, [props.note])
 
-    const updateNote = () => {
-        APIService.UpdateNote(props.note.id, {content, tags})
-        .then(resp => props.updatedData(resp))
-        .catch(error => console.log(error))
+    const updateNote = () => {   
+        if (content == "" || tags == ""){
+            setShowAlert(true)
+        } else{     
+            APIService.UpdateNote(props.note.id, {content, tags})
+            .then(resp => props.updatedData(resp))
+            .catch(error => console.log(error))
+        }
     }
 
     const createNote = () => {
-        APIService.CreateNote({content, tags})
-        .then(resp => props.createdData(resp))
-        .catch(error => console.log(error))
+        if (content == "" || tags == ""){
+            setShowAlert(true)
+        } else{
+            APIService.CreateNote({content, tags})
+            .then(resp => props.createdData(resp))
+            .catch(error => console.log(error))
+        }
     }
 
     return (
         <div>
+            {
+                showAlert ?
+                <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
+                    Please change your input and try again.
+                </Alert>
+                : null
+            }
             {props.note ? (
                 <div className="mb-3 card-light">
                     <div>
@@ -37,7 +54,7 @@ function Form(props) {
 
                     <div>
                         <label htmlFor='tags' className='form-label'>Tags</label>
-                        <input onChange={(e) => setTags(e.target.value)} value={tags} type="text" className='form-control' placeholder='Please enter tags'/>
+                        <input onChange={(e) => setTags(e.target.value)} value={tags} type="text" className='form-control' placeholder='Please enter tags separated by commas'/>
                     </div>
                     
                     {
